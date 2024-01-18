8e00ce94b12ab99c622e74dd06d3bfe5b7203885
planyist <planyist@gmail.com>
2024-01-19 01:13:15 +0900

    API 기능 구현

--- src/main/java/org/example/service/TaskService.java
+++ src/main/java/org/example/service/TaskService.java
@@ -2,6 +2,7 @@ package org.example.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
 import org.example.constants.TaskStatus;
 import org.example.model.Task;
 import org.example.persist.TaskRepository;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -54,6 +56,34 @@ public class TaskService {
     public Task getOne(Long id) {
         var entity = this.getById(id);
         return this.entityToObject(entity);
+    }
+
+    public Task update(Long id, String title, String description, LocalDate dueDate) {
+        var entity = this.getById(id);
+        entity.setTitle(Strings.isEmpty(title) ? entity.getTitle() : title);
+        entity.setDescription(Strings.isEmpty(description) ? entity.getDescription() : description);
+        entity.setDueDate(Objects.isNull(dueDate) ? entity.getDueDate() : Date.valueOf(dueDate));
+
+        var updated = this.taskRepository.save(entity);
+        return this.entityToObject(updated);
+    }
+
+    public Task updateStatus(Long id, TaskStatus status) {
+        var entity = this.getById(id);
+        entity.setStatus(status);
+
+        var updated = this.taskRepository.save(entity);
+        return this.entityToObject(updated);
+    }
+
+    public boolean delete(Long id) {
+        try {
+            this.taskRepository.deleteById(id);
+        } catch(Exception e) {
+            log.error("an error occurred while deleting [{}]", e.toString());
+            return false;
+        }
+        return true;
     }
 
     private TaskEntity getById(Long id) {
